4d7392fe68a6582eae682f3c2533f49943a97574
Rahat-Kibria <rahatkibriawahee@gmail.com>
2024-02-08 04:10:36 +0600

    completed project

---
+++ README.md
@@ -0,0 +1,6 @@
+## About To-Do List project
+
+<ul>
+    <li>HTML, CSS, jQuery are used</li>
+    <li>Followed "Udemy"> "The Web Developer Bootcamp 2020" > "Todo List Projects".</li>
+</ul>
---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("ul").on("click", "li", function () {
+        $(this).toggleClass("completed");
+    });
+
+    $("ul").on("click", "span", function (event) {
+        $(this).parent().fadeOut(500, function () {
+            $(this).remove();
+        });
+        event.stopPropagation();
+    });
+
+    $("input[type='text']").keypress(function (event) {
+        if(event.which === 13) {
+            let todoText = $(this).val();
+            $(this).val("");
+            $("ul").append("<li><span><i class='fa-solid fa-trash-can'></i></span> "+ todoText +"</li>");
+        }
+    });
+
+    $(".fa-plus").click(function () {
+        $("input[type='text']").fadeToggle();
+    });
+});
